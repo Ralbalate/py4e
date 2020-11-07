6c8c7c1d36584618315c970de9c6936e47eac1f0
Edgar Albalate <dev.gardo@gmail.com>
2020-11-07 16:54:27 +0100

    Changed vars names in the second block

--- billeteador.py
+++ billeteador.py
@@ -38,32 +38,32 @@ if resto != 0:
                                 print("monedas de 1: ", cantidad)
 
 print("quedan este resto", resto)
-X = str(input("quieres calculas la calderilla S/N: "))
-if X != "S":
+seguir = str(input("quieres calculas la calderilla S/N: "))
+if seguir != "S":
     print("gracias por participar")
-if X == "S" or "s":
-    P = int(resto * 100)
-    t = P // 50
-    u = P % 50
-    print(P)
-    print("monedas de 0.50: ", t)
-    if u != 0:
-        v = u // 20
-        w = u % 20
-        print("monedas de 0.20: ", v)
-        if w != 0:
-            y = w // 10
-            z = w % 10
-            print("monedas de 0.10: ", y)
-            if z != 0:
-                A = z // 5
-                B = z % 5
-                print("monedas de 0.05: ", A)
-                if B != 0:
-                    C = B // 2
-                    D = B % 2
-                    print("monedas de 0.02: ", C)
-                    if D != 0:
-                        E = D % 1
-                        F = D // 1
-                        print("monedas de 0.01: ", F)
+if seguir == "S" or "s":
+    resto = int(resto * 100)
+    print(resto)
+    cantidad = resto // 50
+    resto = resto % 50
+    print("monedas de 0.50: ", cantidad)
+    if  != 0:
+        cantidad = resto // 20
+        resto = resto % 20
+        print("monedas de 0.20: ", cantidad)
+        if resto != 0:
+            cantidad = resto // 10
+            resto = resto % 10
+            print("monedas de 0.10: ", cantidad)
+            if resto != 0:
+                cantidad = resto // 5
+                resto = resto % 5
+                print("monedas de 0.05: ", cantidad)
+                if resto != 0:
+                    cantidad = resto // 2
+                    resto = resto % 2
+                    print("monedas de 0.02: ", cantidad)
+                    if resto != 0:
+                        cantidad = resto // 1
+                        resto = resto % 1
+                        print("monedas de 0.01: ", cantidad)
