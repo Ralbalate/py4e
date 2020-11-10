8e3134fefa8b1d4153c8ec99abc3694c80a0cb3e
Roberto Albalate <albalaterober@gmail.com>
2020-11-10 12:11:34 +0100

    ahorasi

--- billeteador.py
+++ billeteador.py
@@ -3,18 +3,19 @@ print('programa para calcular billetes')
 #variables
 cantidad=float(input('valor de a: '))
 decimales =int( cantidad *100 % 100)
-entero = int(cantidad)
+resto = int(cantidad)
 #condicion centimos
 if decimales>0:
     print('tienes estos centimos', int (decimales))
     x=str (input('quieres calcular la calderilla? S/N: '))
-if x!='S' and x!='s':
-    decimales=0
+    if x!='S' and x!='s':
+        decimales=0
 
  #calculo de enteros
-if entero>500:
-    a = entero // 500
-    resto = entero % 500
+
+if resto>500:
+    a = resto // 500
+    resto = resto % 500
     print('billetes de 500: ', a)
 if resto==0 and decimales==0:
     exit()
@@ -71,39 +72,40 @@ if decimales==0:
     exit()
 
  #calculo de decimales
+
 if decimales>=50:
     b = decimales // 50
-    resto = decimales % 50
+    decimales = decimales % 50
     print('monedas de 0.50: ', b)
-if resto==0:
+if decimales==0:
     exit()
-if resto>=20:
-    b = resto // 20
-    resto = resto % 20
+if decimales>=20:
+    b = decimales // 20
+    decimales = decimales % 20
     print('monedas de 0.20: ',b)
-if resto==0:
+if decimales==0:
     exit()
-if resto>=10:
-    b = resto // 10
-    resto = resto % 10
+if decimales>=10:
+    b = decimales // 10
+    decimales = decimales % 10
     print('monedas de 0.10: ',b)
-if resto==0:
+if decimales==0:
     exit()
-if resto>=5:
-    b = resto // 5
-    resto = resto % 5
+if decimales>=5:
+    b = decimales // 5
+    decimales = decimales % 5
     print('monedas de 0.05: ',b)
-if resto==0:
+if decimales==0:
     exit()
-if resto>=2:
-    b = resto // 2
-    resto = resto % 2
+if decimales>=2:
+    b = decimales // 2
+    decimales = decimales % 2
     print('monedas de 0.02: ',b)
-if resto==0:
+if decimales==0:
     exit()
-if resto>=1: 
-    b = resto // 1
-    resto = resto % 1
+if decimales>=1: 
+    b = decimales // 1
+    decimales = decimales % 1
     print('monedas de 0.01: ',b)
 
 
